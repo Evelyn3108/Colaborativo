7d127806444d20ea5b9f3b0ec58793981a764a82
Percy64 <104700336+Percy64@users.noreply.github.com>
2025-06-04 21:53:41 -0300

    segundo

---
+++ registro.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>registro</title>
+</head>
+<body>
+    <main>
+        <form class="" action="" method="post">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+            <br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            <br>
+            <button type="submit">Register</button>
+        </form>
+    </main>
+</body>
+</html>
